69e274073d5a70c399f8cd0ec5a5d54e90b8105e
Buldin Alexandr <buldin.alieksandr@gmail.com>
2022-02-01 14:36:24 +0300

    add banal authentification

--- src/main/java/com/mycompany/drontaxi/PrimaryController.java
+++ src/main/java/com/mycompany/drontaxi/PrimaryController.java
@@ -2,11 +2,27 @@ package com.mycompany.drontaxi;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
+    
+    @FXML
+    private TextField userLogin;
+    @FXML
+    private PasswordField userPassword;
+    
 
     @FXML
     private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+        String username = userLogin.getText();
+        String password = userPassword.getText();
+        
+        if(username.equals("Sany") && password.equals("123")){
+          App.setRoot("secondary");  
+        }else{
+            System.out.println("Неправильный логин или пароль!");
+        }
+        
     }
 }
